d4cd68755791bcc04d66ce732f136f5189a87b36
katsuyamurofushi <gahers.14@gmail.com>
2021-04-03 09:13:49 +0900

    課題提出 1-6 js
    index.html
    question1.js
    question2.js

---
+++ JavaScript/1-6/question1.js
@@ -0,0 +1,7 @@
+let number = [10, 15, 20, 25];
+console.log(number);
+for (let i = 0; i < number.length; i++) {
+    if (number[i] % 2 == 0){
+        console.log(number[i] + "は偶数です");
+    }
+}
---
+++ JavaScript/1-6/question2.js
@@ -0,0 +1,7 @@
+let car = {
+    gass: 20.5,
+    num: 1234
+};
+console.log(car);
+console.log("ガソリンは"+car.gass+"です");
+console.log("ナンバーは"+car.num+"です");
