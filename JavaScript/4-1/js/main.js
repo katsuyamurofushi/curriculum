f3e6d1291d16f44d4bed6600bf57b225699567e9
katsuyamurofushi <gahers.14@gmail.com>
2021-05-23 09:08:18 +0900

    js 4章途中経過
    Task.html
    main.js

--- JavaScript/4-1/js/main.js
+++ JavaScript/4-1/js/main.js
@@ -3,7 +3,8 @@ var app = new Vue({
     data: {
         list: [],
         addText: '',
-        message: '現在タスクはありません',
+        task: 0,
+        totalTask: 0,
     },
     //watchでlistの変更を監視
     watch: {
@@ -28,17 +29,30 @@ var app = new Vue({
                 });
             }
             this.addText = '';
-            this.message = '';
         },
         deleteBtn: function() {
             this.list = this.list.filter(function(todo) {
                 return !todo.isChecked;
             });
-            if (this.list == []) {
-                this.message = '現在タスクはありません';
+        },
+        //削除して消すというよりは 「チェックがついていないタスクを返して表示する」 という内容の処理
+        display: function() {
+            if (this.list.length === 0) {
+                return true;
+            }else{
+                return false;
             }
         }
-        //削除して消すというよりは 「チェックがついていないタスクを返して表示する」 という内容の処理
-        
+    },
+    computed: {
+        Counttotal: function() {
+            this.totalTask = this.list.length;
+            return this.totalTask;
+        },
+        Counttask: function() {
+            return this.totalTask;
+            //リスト要素数に比例
+            //リスト要素のcheckedの回答に変動
+        }
     }
 });
